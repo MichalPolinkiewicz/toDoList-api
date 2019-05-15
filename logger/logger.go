84f8a6ac0075e126c8ca5babad612d092fa7cd1f
Michal.Polinkiewicz <Michal.Polinkiewicz@empirica.pl>
2019-05-15 15:22:42 +0200

    add logger to requests

---
+++ logger/logger.go
@@ -0,0 +1,15 @@
+package logger
+
+import (
+	"fmt"
+	"net/http"
+	"time"
+)
+
+//decorator function
+func Log(h http.Handler, url string) http.Handler {
+	return http.HandlerFunc(func(res http.ResponseWriter, req *http.Request) {
+		fmt.Println(time.Now(), url)
+		h.ServeHTTP(res, req)
+	})
+}
